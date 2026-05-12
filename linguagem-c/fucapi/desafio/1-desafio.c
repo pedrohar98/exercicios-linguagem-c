1a4811901041d5f66a98e37b97abf5cad3241782
Pedro Henrique - EXPKAM <pedrohar.1998@gmail.com>
2026-05-12 11:33:59 -0400

    Finalizado a estrutura do desafio

--- linguagem-c/fucapi/desafio/1-desafio.c
+++ linguagem-c/fucapi/desafio/1-desafio.c
@@ -1,63 +1,66 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+    #include <stdio.h>
+    #include <stdlib.h>
+    #include <string.h>
 
-int main(){
+    int main(){
 
-    int option, count = 1;
-    char code[2];
-    char number[3];
-    char product[20];
-    char value[5];
-    char report[1000], line[200]; 
+        int option, count = 1;
+        char code[10];
+        char product[20];
+        char value[5];
+        char report[1000], line[200]; 
 
-    do {
-        printf("\n====== MENU DE OPCOES ======\n");
-        printf("[1] - Cadastrar novo produto\n");
-        printf("[2] - Mostrar relatorio\n");
-        printf("[0] - Sair\n");
-        printf("\nSelecione uma opcao: ");
+        do {
+            printf("\n====== MENU DE OPCOES ======\n");
+            printf("[1] - Cadastrar novo produto\n");
+            printf("[2] - Mostrar relatorio\n");
+            printf("[0] - Sair\n");
+            printf("\nSelecione uma opcao: ");
 
-        scanf("%d", &option);
-        getchar(); //captura o enter de confirmação das options
+            scanf("%d", &option);
+            getchar(); //captura o enter de confirmação das options
 
-        switch (option){
-            case 1:
-                printf("\nDigite o codigo do produto: ");
-                gets(code);
+            switch (option){
+                case 1:
+                    printf("\nDigite o codigo do produto [AW123]: ");
+                    gets(code);
 
-                printf("Digite o nome do produto: ");
-                gets(product);
+                    printf("\nDigite o nome do produto: ");
+                    gets(product);
 
-                printf("Digite o Valor: RS: ");
-                gets(value);
+                    printf("\nDigite o Valor: RS: ");
+                    gets(value);
+                    
+                    strcat(line, "");
+                    strcat(line, code);
+                    strcat(line, " - ");
+                    strcat(line, product);
+                    strcat(line, " - ");
+                    strcat(line, value);
+                    strcat(line, "\n");
+
+                    strcat(report, line);
+                    printf("\nProduto cadastrado com sucesso!\n");
+                break;
+                    
+                case 2:
+                    if (strlen(report) == 0) {
+                        printf("\nNao ha dados no relatorio\n");
+                    } else {
+                        printf("\n====== RELATORIO DE PRODUTOS ======\n");
+                        printf("\n%s\n", report);
+                    }
+                break;
+
+                case 0:
+                count = 0;
+                break;
                 
-                strcat(line, "");
-                strcat(line, code);
-                strcat(line, " - ");
-                strcat(line, product);
-                strcat(line, " - ");
-                strcat(line, value);
-
-                strcat(report, line);
-            break;
-                
-            case 2:
-                if (strcmp (report, line) == 0) {
-                    printf("\nNao ha dados no relatorio\n");
-                } else {
-
-                }
-            break;
-
-            case 0:
-            return 0;
-            
-            default:
-                printf("\nValor invalido\n");
-            break;
-        }
-    } while (count != 0);
-    
-    return 0;
-}
+                default:
+                    printf("\nValor invalido\n");
+                break;
+            }
+        } while (count != 0);
+        
+        return 0;
+    }
